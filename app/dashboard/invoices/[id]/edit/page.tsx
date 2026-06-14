dad923ec5a41ac480b2f19079c28733ce54b54ae
Dmitrii Cherednikov <d.cherednikov@itq-group.com>
2026-06-14 10:05:34 +0300

    added actions for invoices

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,35 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+
+type Params = Promise<{ id: string }>;
+
+export default async function Page({
+  params,
+}: {
+  params: Params;
+}) {
+  const newParams = await params;
+  const id = newParams.id;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
