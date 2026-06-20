6ac519903e1d61bb666e683ddecf5043cc1cd779
Dmitrii Cherednikov <d.cherednikov@itq-group.com>
2026-06-20 14:58:35 +0300

    added error handlers and lint config

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 type Params = Promise<{ id: string }>;
 
@@ -16,6 +17,10 @@ export default async function Page({
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
